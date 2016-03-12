8ab27155ae5ae394141a5d3edf7c0c6787f7977d
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2016-03-12 14:08:15 +0100

    Started google api

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/TetAlius/GoSyncMyCalendars/backend/google"
+)
+
+func main() {
+	//Parse configuration of outlook and google
+	file, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = json.Unmarshal(file, &google.Config)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//Parse all requests for google
+	file, err = ioutil.ReadFile("./google.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = json.Unmarshal(file, &google.Requests)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	http.HandleFunc("/", welcomeHandler)
+}
