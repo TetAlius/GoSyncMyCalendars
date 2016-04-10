9d9d2cde519cae3cca161a6d4f06f1a56352b25d
Marta Jiménez Franco <tetaliuss@gmail.com>
2016-04-10 18:42:14 +0200

    Database implemented
    sort of, only calendars and it is not finished yet

---
+++ database/db.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+//SaveUser saves the user data onto the DB
+func SaveUser(db *sql.DB) (err error) {
+	row, err := db.Query(`INSERT INTO users(mail, diggested_password) VALUES('marta@marta.com', 'password')`)
+	fmt.Println(row)
+	return
+}
+
+//CreateEvent TODO
+func CreateEvent(db *sql.DB, idCal string, id string, mail string) {
+
+}
+
+//UpdateEvent TODO
+func UpdateEvent(db *sql.DB, id string, info interface{}) {
+
+}
+
+//GetEvent TODO
+func GetEvent(db *sql.DB, id string) {
+
+}
+
+//DeleteEvent TODO
+func DeleteEvent(db *sql.DB, id string) {
+
+}
