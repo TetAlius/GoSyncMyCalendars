1ebb4b888e076bde645358ffe1d3fed1b0ba1b48
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2017-07-04 20:05:37 +0200

    Finish start of outlook handler

--- frontend/handlers/outlook.go
+++ frontend/handlers/outlook.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	//"github.com/TetAlius/GoSyncMyCalendars/backend/google"
 	"encoding/json"
+	"github.com/TetAlius/GoSyncMyCalendars/customErrors"
 	"io/ioutil"
 )
 
@@ -23,20 +24,26 @@ type outlookConfig struct {
 	Scope       string `json:"scope"`
 }
 
-func NewOutlookHandler() *Outlook {
-
-	outlook := Outlook{}
-
-	//Parse configuration of google
-	file, err := ioutil.ReadFile("./config.json")
+func NewOutlookHandler(fileName *string) (outlook *Outlook, err error) {
+	outlook = &Outlook{}
+	//Parse configuration of outlook
+	//file, err := ioutil.ReadFile("./config.json")
+	file, err := ioutil.ReadFile(*fileName)
 	if err != nil {
-		log.Fatalf("Error reading config.json: %s", err.Error())
+		log.Errorf("Error reading config.json: %s", err.Error())
+		return
 	}
 	err = json.Unmarshal(file, &outlook)
 	if err != nil {
-		log.Fatalf("Error unmarshalling google config: %s", err.Error())
+		log.Errorf("Error unmarshalling google config: %s", err.Error())
+		return
 	}
-	return &outlook
+	if len(outlook.ID) == 0 || len(outlook.Secret) == 0 || len(outlook.RedirectURI) == 0 || len(outlook.LoginURI) == 0 || len(outlook.Version) == 0 || len(outlook.Scope) == 0 {
+		err = customErrors.ConfigNotChargedCorrectlyError{Message: "Outlook Config has not loaded properly"}
+		log.Errorf("%s", err.Error())
+		return
+	}
+	return outlook, err
 }
 
 func (o *Outlook) SignInHandler(w http.ResponseWriter, r *http.Request) {
@@ -44,5 +51,5 @@ func (o *Outlook) SignInHandler(w http.ResponseWriter, r *http.Request) {
 		o.LoginURI+o.Version+
 			"/authorize?client_id="+o.ID+
 			"&redirect_uri="+o.RedirectURI+
-			"&response_type=code&scope="+o.Scope, 301)
+			"&response_type=code&scope="+o.Scope, 302)
 }
