fad5803c28f662506f81b139d15e194c0372e82b
Marta Jiménez Franco <tetaliuss@gmail.com>
2017-07-04 20:03:25 +0200

    Finish start of google Handler

--- frontend/handlers/google.go
+++ frontend/handlers/google.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	//"github.com/TetAlius/GoSyncMyCalendars/backend/google"
 	"encoding/json"
+	"github.com/TetAlius/GoSyncMyCalendars/customErrors"
+	"html/template"
 	"io/ioutil"
 )
 
@@ -22,27 +24,33 @@ type googleConfig struct {
 	Scope         string `json:"scope"`
 }
 
-func NewGoogleHandler() *Google {
-
-	google := Google{}
-
+func NewGoogleHandler(fileName *string) (google *Google, err error) {
+	google = &Google{}
 	//Parse configuration of google
-	file, err := ioutil.ReadFile("./config.json")
+	//file, err := ioutil.ReadFile("./config.json")
+	file, err := ioutil.ReadFile(*fileName)
 	if err != nil {
-		log.Fatalf("Error reading config.json: %s", err.Error())
+		log.Errorf("Error reading config.json: %s", err.Error())
+		return
 	}
 	err = json.Unmarshal(file, &google)
 	if err != nil {
-		log.Fatalf("Error unmarshalling google config: %s", err.Error())
+		log.Errorf("Error unmarshalling google config: %s", err.Error())
+		return
 	}
-	return &google
+
+	if len(google.ID) == 0 || len(google.Secret) == 0 || len(google.RedirectURI) == 0 || len(google.Endpoint) == 0 || len(google.TokenEndpoint) == 0 || len(google.Scope) == 0 {
+		err = customErrors.ConfigNotChargedCorrectlyError{Message: "Google Config has not loaded properly"}
+		log.Errorf("%s", err.Error())
+		return
+	}
+	return google, err
 }
 
 //SingInHandler Google SingIn handler
 func (g *Google) SignInHandler(w http.ResponseWriter, r *http.Request) {
 	//google.Requests.State = google.GenerateRandomState()
 	//log.Debugf("Random google state: %s", google.Requests.State)
-
 	http.Redirect(w, r, g.Endpoint+
 		"?client_id="+g.ID+
 		"&access_type=offline&response_type=code"+
@@ -50,5 +58,17 @@ func (g *Google) SignInHandler(w http.ResponseWriter, r *http.Request) {
 		"&redirect_uri="+g.RedirectURI+
 		//"&state="+g.State+ //TODO Uncomment
 		"&prompt=consent&include_granted_scopes=true",
-		301)
+		302)
+}
+
+func (g *Google) TokenHandler(w http.ResponseWriter, r *http.Request) {
+	log.Debugln("Has entered token handler")
+	t, err := template.ParseFiles("./frontend/resources/html/welcome.html")
+	if err != nil {
+		log.Errorln("Error reading config.json: %s", err.Error())
+	}
+	err = t.Execute(w, nil) //No template at this moment
+	if err != nil {
+		log.Errorln(err)
+	}
 }
