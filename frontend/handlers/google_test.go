3f9d440abf26fe15c1dfe1d5c5889db233676979
Marta Jiménez Franco <tetaliuss@gmail.com>
2017-07-04 20:06:56 +0200

    Google testing

--- frontend/handlers/google_test.go
+++ frontend/handlers/google_test.go
@@ -6,3 +6,23 @@ import "testing"
 func TestSignInHandler(t *testing.T) {
 	t.Log("Testing Travis and Codecov")
 }
+//
+//	//Seccion mala
+//
+//	configNameErr := "configgggg.json"
+//	configNameGood := "../../config.json"
+//
+//	f := frontend.NewFrontend("127.0.0.1", 8080, &configNameErr)
+//	f.Start()
+//	m.Run()
+//
+//	f.Stop()
+//
+//	log.Debugln("Main")
+//	f = frontend.NewFrontend("127.0.0.1", 8080, &configNameGood)
+//	f.Start()
+//	m.Run()
+//	f.Stop()
+//
+//	//Seccion buena
+//}
