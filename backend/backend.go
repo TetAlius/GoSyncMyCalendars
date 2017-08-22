bf9b9da02b3cb3ec340775a6018c4708147b032e
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2017-08-22 19:08:49 +0200

    Start backend with google handler

--- backend/backend.go
+++ backend/backend.go
@@ -5,27 +5,36 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/TetAlius/GoSyncMyCalendars/backend/handlers"
 	log "github.com/TetAlius/GoSyncMyCalendars/logger"
 )
 
 //Backend object
 type Backend struct {
-	IP   net.IP
-	Port int
+	IP            net.IP
+	Port          int
+	googleHandler *handlers.Google
+	//outlookHandler *handlers.Outlook
 }
 
 //NewBackend creates a backend
 func NewBackend(ip string, port int) *Backend {
-	backend := Backend{net.ParseIP(ip), port}
+	googleHandler := handlers.NewGoogleHandler()
+	backend := Backend{net.ParseIP(ip), port, googleHandler}
 	return &backend
 }
 
 //Start the backend
 func (b *Backend) Start() {
 	log.Debugln("Start backend")
+	webServerMux := http.NewServeMux()
+
+	webServerMux.HandleFunc("/google", b.googleHandler.TokenHandler)
+
 	laddr := b.IP.String() + ":" + strconv.Itoa(b.Port)
 	log.Infof("Backend server listening at %s", laddr)
-	err := http.ListenAndServe(laddr, nil)
+	http.HandleFunc("/google", b.googleHandler.TokenHandler)
+	err := http.ListenAndServe(laddr, webServerMux)
 	if err != nil {
 		log.Fatalf("ListenAndServe: " + err.Error())
 	}
