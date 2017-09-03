47023f677743103cb79477bfabc705b7c3f7e51e
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2017-09-03 20:00:31 +0200

    Test Primary calendar for outlook

--- backend/outlook/calendar_test.go
+++ backend/outlook/calendar_test.go
@@ -1,1 +1,26 @@
 package outlook_test
+
+import (
+	"os"
+	"testing"
+)
+
+func TestOutlookAccount_GetPrimaryCalendar(t *testing.T) {
+	account := setup()
+	//Refresh previous petition in order to have tokens updated
+	account.Refresh()
+
+	err := account.GetPrimaryCalendar()
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
+	}
+
+	os.Setenv("API_ROOT", "")
+	// Bad calling to GetPrimaryCalendar
+	err = account.GetPrimaryCalendar()
+	if err == nil {
+		t.Fail()
+	}
+
+}
