decb66cb10901b5282f2d2c16e017a21f03f5b51
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2017-09-03 19:27:54 +0200

    Finished testing outlook backend

--- backend/outlook/outlook_test.go
+++ backend/outlook/outlook_test.go
@@ -5,9 +5,38 @@ import (
 
 	"testing"
 
+	"encoding/json"
+
 	"github.com/TetAlius/GoSyncMyCalendars/backend/outlook"
 	"github.com/TetAlius/GoSyncMyCalendars/logger"
 )
+
+func TestNewAccount(t *testing.T) {
+	// Bad Info inside Json
+	b := []byte(`{"Name":"Bob","Food":"Pickle"}`)
+	_, err := outlook.NewAccount(b)
+	if err == nil {
+		t.Fail()
+	}
+
+	//Bad formatted JSON
+	b = []byte(`{"id_token":"ASD.ASD.ASD","Food":"Pickle"`)
+	_, err = outlook.NewAccount(b)
+	if err == nil {
+		t.Fail()
+	}
+
+	//Correct information given
+	account := setup()
+	b, err = json.Marshal(account)
+	if err != nil {
+		t.Fail()
+	}
+	_, err = outlook.NewAccount(b)
+	if err != nil {
+		t.Fail()
+	}
+}
 
 func TestOutlookAccount_Refresh(t *testing.T) {
 	//Empty initialized info account
@@ -21,9 +50,16 @@ func TestOutlookAccount_Refresh(t *testing.T) {
 	//Good info account
 	account = setup()
 	err = account.Refresh()
+	if err != nil {
+		t.Fail()
+	}
+
+	os.Setenv("API_ROOT", "")
+
+	err = account.Refresh()
 	logger.Debugln(err)
 
-	if err != nil {
+	if err == nil {
 		t.Fail()
 	}
 
