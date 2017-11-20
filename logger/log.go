bdae31ce69f8e9787e8ee053ed5c231ffae2ac88
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2017-11-20 18:14:14 +0100

    environment testing

--- logger/log.go
+++ logger/log.go
@@ -120,6 +120,12 @@ func init() {
 		warningHandle = os.Stdout
 		errorHandle = os.Stdout
 		fatalHandle = os.Stdout
+	case "testing-travis":
+		debugHandle = ioutil.Discard
+		infoHandle = ioutil.Discard
+		warningHandle = ioutil.Discard
+		errorHandle = ioutil.Discard
+		fatalHandle = ioutil.Discard
 	default:
 		fd, err := os.OpenFile("./gsmc-develop.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 		if err != nil {
