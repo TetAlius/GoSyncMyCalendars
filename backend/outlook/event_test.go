cb87047ec240266580127998017ff1691aadf3b4
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2017-11-20 19:44:04 +0100

    Test function for outlook events

--- backend/outlook/event_test.go
+++ backend/outlook/event_test.go
@@ -1,1 +1,28 @@
 package outlook_test
+
+import "testing"
+
+func TestAccount_GetAllEventsFromCalendar(t *testing.T) {
+	t.Fail()
+	t.Fatal("Not implemented yet")
+}
+
+func TestAccount_CreateEvent(t *testing.T) {
+	t.Fail()
+	t.Fatal("Not implemented yet")
+}
+
+func TestAccount_GetEvent(t *testing.T) {
+	t.Fail()
+	t.Fatal("Not implemented yet")
+}
+
+func TestAccount_UpdateEvent(t *testing.T) {
+	t.Fail()
+	t.Fatal("Not implemented yet")
+}
+
+func TestAccount_DeleteEvent(t *testing.T) {
+	t.Fail()
+	t.Fatal("Not implemented yet")
+}
