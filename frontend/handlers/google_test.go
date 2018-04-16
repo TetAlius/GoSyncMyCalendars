72ca37b4f3a5712dce342aee1d72487f14e7528c
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2018-04-16 20:40:37 +0200

    Fix tests

--- frontend/handlers/google_test.go
+++ frontend/handlers/google_test.go
@@ -7,15 +7,11 @@ import (
 	"os"
 
 	"github.com/TetAlius/GoSyncMyCalendars/frontend"
-	log "github.com/TetAlius/GoSyncMyCalendars/logger"
 )
-
-func init() {
-	os.Setenv("API_ROOT", os.Getenv("API_ROOT_TEST"))
-}
 
 //TestGoogleSignInHandler test the SignInHandler method
 func TestGoogleSignInHandler(t *testing.T) {
+	setupApiRoot()
 	f := frontend.NewServer("127.0.0.1", 8080)
 	f.Start()
 	defer f.Stop()
@@ -24,10 +20,10 @@ func TestGoogleSignInHandler(t *testing.T) {
 	// Check if you received the status codes you expect. There may
 	// status codes other than 200 which are acceptable.
 	if resp.StatusCode != 200 && resp.StatusCode != 302 {
-		log.Errorf("Failed with status %d %s", resp.StatusCode, resp.Status)
 		t.Fail()
+		t.Fatalf("Failed with status %d %s", resp.StatusCode, resp.Status)
+		return
 	}
-	log.Infoln(resp.Header.Get("Location"))
 
 	os.Setenv("API_ROOT", "")
 	// Bad request
@@ -35,28 +31,34 @@ func TestGoogleSignInHandler(t *testing.T) {
 	// Check if you received the status codes you expect. There may
 	// status codes other than 200 which are acceptable.
 	if resp.StatusCode != 500 && resp.Header.Get("Location") != "htpp://localhost:8080/" {
-		log.Errorf("Failed with status %d %s", resp.StatusCode, resp.Status)
 		t.Fail()
+		t.Fatalf("Failed with status %d %s", resp.StatusCode, resp.Status)
+		return
 	}
 
 	os.Setenv("API_ROOT", os.Getenv("API_ROOT_TEST"))
 }
 
-func requestGoogleSignIn(t *testing.T) *http.Response {
+func requestGoogleSignIn(t *testing.T) (resp *http.Response) {
 	// Set up the HTTP request
 	req, err := http.NewRequest("GET", "http://127.0.0.1:8080/SignInWithGoogle", nil)
 	if err != nil {
-		log.Errorln(err.Error())
 		t.Fail()
+		t.Fatal(err.Error())
+		return
 	}
 
 	transport := http.Transport{}
-	resp, err := transport.RoundTrip(req)
+	resp, err = transport.RoundTrip(req)
 
 	if err != nil {
-		log.Errorln(err.Error())
 		t.Fail()
+		t.Fatal(err.Error())
+		return
 	}
 
 	return resp
+}
+func setupApiRoot() {
+	os.Setenv("API_ROOT", os.Getenv("API_ROOT_TEST"))
 }
--- frontend/handlers/outlook_test.go
+++ frontend/handlers/outlook_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/TetAlius/GoSyncMyCalendars/frontend"
-	log "github.com/TetAlius/GoSyncMyCalendars/logger"
 )
 
 func init() {
@@ -15,6 +14,7 @@ func init() {
 
 //TestGoogle_SignInHandlerSignInHandler test the SignInHandler method
 func TestOutlookSignInHandler(t *testing.T) {
+	setupApiRoot()
 	f := frontend.NewServer("127.0.0.1", 8080)
 	f.Start()
 	defer f.Stop()
@@ -24,38 +24,42 @@ func TestOutlookSignInHandler(t *testing.T) {
 	// Check if you received the status codes you expect. There may
 	// status codes other than 200 which are acceptable.
 	if resp.StatusCode != 200 && resp.StatusCode != 302 {
-		log.Errorf("Failed with status %d %s", resp.StatusCode, resp.Status)
 		t.Fail()
+		t.Fatalf("Failed with status %d %s", resp.StatusCode, resp.Status)
+		return
 	}
-	log.Infoln(resp.Header.Get("Location"))
+	t.Log(resp.Header.Get("Location"))
 
 	os.Setenv("API_ROOT", "")
 	// Bad request
 	resp = requestOutlookSignIn(t)
-	log.Infoln(resp.Header.Get("Location"))
+	t.Log(resp.Header.Get("Location"))
 	// Check if you received the status codes you expect. There may
 	// status codes other than 200 which are acceptable.
 	if resp.StatusCode != 500 && resp.Header.Get("Location") != "htpp://localhost:8080/" {
-		log.Errorf("Failed with status %d %s", resp.StatusCode, resp.Status)
 		t.Fail()
+		t.Fatalf("Failed with status %d %s", resp.StatusCode, resp.Status)
+		return
 	}
 
 	os.Setenv("API_ROOT", os.Getenv("API_ROOT_TEST"))
 }
-func requestOutlookSignIn(t *testing.T) *http.Response {
+func requestOutlookSignIn(t *testing.T) (resp *http.Response) {
 	// Set up the HTTP request
 	req, err := http.NewRequest("GET", "http://127.0.0.1:8080/SignInWithOutlook", nil)
 	if err != nil {
-		log.Errorln(err.Error())
 		t.Fail()
+		t.Fatal(err.Error())
+		return
 	}
 
 	transport := http.Transport{}
-	resp, err := transport.RoundTrip(req)
+	resp, err = transport.RoundTrip(req)
 
 	if err != nil {
-		log.Errorln(err.Error())
 		t.Fail()
+		t.Fatal(err.Error())
+		return
 	}
 
 	return resp
