7f4fa1ddd63ff89173c674bac13a864e91a78212
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2018-05-30 14:51:24 +0200

    Delete all events related to subscription from db

--- backend/db/event.go
+++ backend/db/event.go
@@ -23,7 +23,7 @@ func (data Database) savePrincipalEvents(transaction *sql.Tx, events []api.Event
 		case err != nil:
 			data.sentry.CaptureErrorAndWait(err, map[string]string{"database": "backend"})
 			log.Debugf("error insert event with id: %s and calendar UUID: %s", event.GetID(), event.GetCalendar().GetUUID())
-			return
+			return err
 		}
 		event.SetInternalID(lastInsertId)
 	}
@@ -54,10 +54,37 @@ func (data Database) saveEventsRelation(transaction *sql.Tx, from api.EventManag
 		log.Errorf("error retrieving rows affected: %s", err.Error())
 		return
 	}
+	//TODO: Change this...
 	if affect != 1 {
 		err = errors.New(fmt.Sprintf("could not update account with id: %s", to.GetID()))
 		data.sentry.CaptureErrorAndWait(err, map[string]string{"database": "backend"})
 		return
 	}
 	return
+}
+
+func (data Database) deleteEventsFromSubscription(transaction *sql.Tx, subscription api.SubscriptionManager) (err error) {
+	stmt, err := transaction.Prepare("delete from events using subscriptions, calendars where subscriptions.uuid = $1 and subscriptions.calendar_uuid = calendars.uuid and events.calendar_uuid = calendars.uuid")
+	if err != nil {
+		data.sentry.CaptureErrorAndWait(err, map[string]string{"database": "backend"})
+		log.Errorf("error preparing statement: %s", err.Error())
+		return
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(subscription.GetUUID())
+	if err != nil {
+		data.sentry.CaptureErrorAndWait(err, map[string]string{"database": "backend"})
+		log.Errorf("error executing query: %s", err.Error())
+		return
+	}
+	rows, err := result.RowsAffected()
+	if err != nil {
+		data.sentry.CaptureErrorAndWait(err, map[string]string{"database": "backend"})
+		log.Errorf("error getting rows affected: %s", err.Error())
+	}
+	log.Infof("deleted %d events from subscription: %s", rows, subscription.GetUUID())
+
+	return
+
 }
