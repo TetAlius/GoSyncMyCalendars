b10e7cbcce5d28b057e051a3bb252d3ac4c402f5
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2018-11-21 12:28:37 +0100

    Update method to test
    In order to fix for sonar

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,12 +6,13 @@
 set -e
 cd "$(dirname "$0")/.."
 
-echo "" > coverage.txt
+go test -race -coverprofile=coverage.txt -covermode=atomic ./...
+#echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic $d
-    if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
-        rm profile.out
-    fi
-done
+#for d in $(go list ./... | grep -v vendor); do
+#    go test -race -coverprofile=profile.out -covermode=atomic $d
+#    if [ -f profile.out ]; then
+#        cat profile.out >> coverage.txt
+#        rm profile.out
+#    fi
+#done
